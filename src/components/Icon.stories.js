a1c393631d7b20888f57bd9fce44de0f8d37f59e
Tair Assimov <tair@groovehq.com>
2019-11-22 15:47:20 +0100

    Use emotion instead of styled-components (#2)

--- src/components/Icon.stories.js
+++ src/components/Icon.stories.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 
 import { color, borderColor, spacing } from './shared/styles'
 import { Icon } from './Icon'
