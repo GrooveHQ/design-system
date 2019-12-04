15f7e5f54412ba9801f4302f214a21a6b6403d55
Tair Assimov <tair@groovehq.com>
2019-12-04 14:23:45 +0100

    Add Label component (#12)

---
+++ src/components/Label.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+import { color, spacing, typography } from './shared/styles'
+
+const StyledLabel = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  font-size: ${typography.sizes.p4.size}px;
+  line-height: ${typography.sizes.p4.size}px;
+  padding: ${spacing.padding.mini}px;
+  border-radius: ${spacing.borderRadius.small}px;
+  color: ${color.paperWhite};
+  background: ${props => color[props.color]};
+`
+
+export function Label({ ...props }) {
+  return <StyledLabel {...props} />
+}
+
+Label.propTypes = {
+  /**
+   * Specify color
+   */
+  color: PropTypes.string,
+}
+
+Label.defaultProps = {
+  color: 'primary',
+}
---
+++ src/components/Label.stories.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { Label } from './Label'
+
+import { spacing } from './shared/styles'
+
+export default {
+  title: 'Design System/Label',
+  component: Label,
+}
+
+const Span = styled.span`
+  dispay: inline-block;
+  margin-right: ${spacing.padding.tiny}px;
+`
+
+export const all = () => (
+  <div>
+    <Span>
+      <Label>Primary</Label>
+    </Span>
+    <Span>
+      <Label color="groovy">Groovy</Label>
+    </Span>
+    <Span>
+      <Label color="mintGreen">Mint Green</Label>
+    </Span>
+    <Span>
+      <Label color="sunYellow">Sun Yellow</Label>
+    </Span>
+    <Span>
+      <Label color="candyRed">Candy Red</Label>
+    </Span>
+    <Span>
+      <Label color="jetBlack">Jet Black</Label>
+    </Span>
+    <Span>
+      <Label color="gunGrey">Gun Grey</Label>
+    </Span>
+  </div>
+)
