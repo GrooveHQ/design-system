f52bd621e25954bd5b7e85b062af4e4ab81ae41f
Tair Assimov <tair@groovehq.com>
2020-01-29 13:26:10 +0100

    Support for an Input with a button

--- src/components/Forms/Input.js
+++ src/components/Forms/Input.js
@@ -21,12 +21,37 @@ const StyledIcon = styled(Icon)`
   cursor: ${props => (props.onClick ? 'pointer' : 'auto')};
 `
 
+const ButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: ${props => (props.position === 'right' ? '0px' : 'auto')};
+  left: ${props => (props.position === 'left' ? '0px' : 'auto')};
+
+  button {
+    ${props =>
+      props.position === 'right' &&
+      `
+      border-top-left-radius: 0px;
+      border-bottom-left-radius: 0px;
+    `}
+
+    ${props =>
+      props.position === 'left' &&
+      `
+      border-top-right-radius: 0px;
+      border-bottom-right-radius: 0px;
+    `}
+  }
+`
+
 export const Input = ({
   stretched,
   label: labelText,
   icon,
   iconPosition,
   onIconClick,
+  button,
+  buttonPosition,
   ...rest
 }) => {
   const fieldCtx = useContext(FieldContext)
@@ -49,8 +74,21 @@ export const Input = ({
       ${iconPosition === 'left' ? spacing.padding.big : spacing.padding.small}px;
   `
 
+  const buttonInputStyle = css`
+    padding: 0
+      ${buttonPosition === 'right'
+        ? spacing.padding.big + spacing.padding.small
+        : spacing.padding.small}px
+      0
+      ${buttonPosition === 'left'
+        ? spacing.padding.big + spacing.padding.small
+        : spacing.padding.small}px;
+  `
+
   if (icon) {
     classes.push(iconInputStyle)
+  } else if (buttonPosition) {
+    classes.push(buttonInputStyle)
   }
 
   const handleIconClick = useCallback(() => {
@@ -62,18 +100,21 @@ export const Input = ({
   return (
     <Label stretched={stretched} text={labelText}>
       <input
-        size={labelText.length}
+        size={labelText ? labelText.length : 0}
         placeholder={labelText}
         css={[classes]}
         {...rest}
       />
-      {icon && (
+      {icon && !button && (
         <StyledIcon
           onClick={onIconClick ? handleIconClick : undefined}
           icon={icon}
           position={iconPosition}
           color="metalGrey"
         />
+      )}
+      {button && !icon && (
+        <ButtonWrapper position={buttonPosition}>{button}</ButtonWrapper>
       )}
     </Label>
   )
@@ -97,6 +138,14 @@ Input.propTypes = {
    */
   iconPosition: PropTypes.oneOf(['left', 'right']),
   onIconClick: PropTypes.func,
+  /**
+   * Specify button
+   */
+  button: PropTypes.node,
+  /**
+   * Specify button position
+   */
+  buttonPosition: PropTypes.oneOf(['left', 'right']),
 }
 
 Input.defaultProps = {
@@ -104,4 +153,6 @@ Input.defaultProps = {
   icon: null,
   iconPosition: 'right',
   onIconClick: null,
+  button: null,
+  buttonPosition: 'right',
 }
