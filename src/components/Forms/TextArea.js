1e24fb6f8b6abe2b8aad7abf3bb757bcd547c40e
Jared A. Scheel <jared@jaredscheel.com>
2020-04-27 09:17:36 -0500

    Add react-autosize-textarea to TextArea component

--- src/components/Forms/TextArea.js
+++ src/components/Forms/TextArea.js
@@ -1,8 +1,9 @@
 /** @jsx jsx */
 
-import React, { useContext } from 'react'
+import React, { useContext, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { css, jsx } from '@emotion/core'
+import TextareaAutosize from 'react-autosize-textarea'
 import { FieldContext } from './Field'
 import { Label } from './Label'
 import { spacing, forms } from '../shared/styles'
@@ -24,7 +25,13 @@ export const TextArea = React.forwardRef(
     ]
     return (
       <Label text={labelText}>
-        <textarea {...rest} css={[classes]} placeholder={labelText} ref={ref} />
+        <TextareaAutosize
+          {...rest}
+          css={classes}
+          placeholder={labelText}
+          ref={ref}
+          async
+        />
       </Label>
     )
   }
@@ -35,4 +42,25 @@ TextArea.propTypes = {
    * Label text (also wraps Input in Label component)
    */
   label: PropTypes.string.isRequired,
+
+  /**
+   * Minimum number of visible rows
+   */
+  rows: PropTypes.number,
+
+  /**
+   * Maximum number of visible rows
+   */
+  maxRows: PropTypes.number,
+
+  /**
+   * Called whenever the textarea resizes
+   */
+  onResize: PropTypes.func,
+}
+
+TextArea.defaultProps = {
+  rows: undefined,
+  maxRows: undefined,
+  onResize: undefined,
 }
