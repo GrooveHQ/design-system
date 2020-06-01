aa6f0700e399630d81daa99dd46e655335199283
Jared A. Scheel <jared@jaredscheel.com>
2020-06-01 13:20:06 -0500

    Refactor badge
    - Fix issue where controlled/uncontrolled state becomes out of sync
    - Clean up icon animations
    - Add support for onAnimationComplete
    - Fix text overflow
    - Fix reverse icon/text order
    - Fix text width animation
    - Fix unread count positioning
    - Limit unread count to 99+

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import styled from '@emotion/styled'
+import { motion, AnimatePresence } from 'framer-motion'
 import { Icon } from './Icon'
 import { Paragraph } from './Paragraph'
 import { shadows, spacing } from './shared/styles'
@@ -14,20 +15,18 @@ const SIZES = {
 const StyledBadge = styled.div`
   box-sizing: border-box;
   display: inline-flex;
-  justify-content: center;
   align-items: center;
   user-select: none;
   cursor: pointer;
-  position: relative;
   outline: none;
-  box-shadow: ${props => (props.opened ? shadows.high : shadows.low)};
-
+  box-shadow: ${props => (props.open ? shadows.high : shadows.low)};
   border-radius: ${props => SIZES[props.size]}px;
   background-color: var(--color-primary);
   min-width: ${props => SIZES[props.size]}px;
+  max-width: 320px;
   height: ${props => SIZES[props.size]}px;
-  padding: 0 ${props => (props.text ? 12 : spacing.padding.tiny)}px;
   flex-direction: ${props => (props.reverse ? 'row-reverse' : 'row')};
+  position: relative;
 
   :hover {
     background-color: var(--color-primaryHover);
@@ -38,120 +37,188 @@ const StyledBadge = styled.div`
   }
 `
 
-const StyledIcon = styled(Icon)`
-  margin-right: ${props =>
-    props.text && !props.reverse ? spacing.padding.mini : 0}px;
+const IconContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: ${props => SIZES[props.size]}px;
+  height: ${props => SIZES[props.size]}px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 `
 
-const StyledTextWrapper = styled.div`
+const TextContainer = styled(motion.div)`
+  ${props =>
+    props.icon
+      ? `margin-left: ${SIZES[props.size]}px;`
+      : `margin-left: ${spacing.padding.small}px;`}
   white-space: nowrap;
   overflow: hidden;
-  transition: width ${transition.duration.default.ms.dimension}
-    ${transition.easing.default} 0ms;
-  width: ${props => (props.opened ? 0 : props.width)}px;
 `
 
-const StyledCount = styled.div`
-  opacity: ${props => (props.opened ? 0 : 1)};
+const Text = styled(Paragraph)`
   overflow: hidden;
-  transition: ${props =>
-    props.closing
-      ? `opacity ${transition.duration.default.ms.dimension} ${transition.easing.default} ${transition.duration.default.ms.dimension}`
-      : 'none'};
+  ${props => props.ellipsis && 'text-overflow: ellipsis;'}
+  white-space: nowrap;
+  margin: 0 ${spacing.padding.small}px 0 0;
+`
+
+const Count = styled(motion.custom(Paragraph))`
   border-radius: ${spacing.padding.tiny}px;
   border: 2px solid var(--color-paperWhite);
   background-color: var(--color-candyRed);
-  position: absolute;
-  left: ${props =>
-    props.opened || props.closing
-      ? 0
-      : props.width + SIZES[props.size] / 2 + 14}px;
-  top: 0;
   padding: 0 ${spacing.padding.mini}px;
+  position: absolute;
+  top: 0;
+  left: 100%;
+  transform: translate3d(-${spacing.padding.small}px, 0, 0);
   z-index: 1;
 `
 
-const useTransition = (open, text, onClick) => {
-  const [opened, setOpen] = useState(open)
-  const [width, setWidth] = useState(0)
-  const [closing, setClosing] = useState(open)
+const opacityVariants = {
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: transition.duration.fast.s.number,
+      easing: 'easeIn',
+    },
+  },
+  hidden: {
+    opacity: 0,
+    transition: {
+      duration: transition.duration.fast.s.number,
+      easing: 'easeIn',
+    },
+  },
+}
 
-  const textContentRef = useRef(null)
-
-  const handleClick = useCallback(() => {
-    setOpen(!opened)
-
-    setTimeout(() => {
-      setClosing(!closing)
-    }, 120) // this delay must match width transition delay above
-
-    if (onClick) {
-      onClick()
-    }
-  }, [opened, onClick, closing])
-
-  useEffect(() => {
-    if (text) {
-      const measure = document.createElement('span')
-      measure.innerText = text
-      textContentRef.current.appendChild(measure)
-      const measuredWidth = measure.offsetWidth
-      textContentRef.current.removeChild(measure)
-      setWidth(measuredWidth > 0 ? measuredWidth + spacing.padding.tiny : 0) // including padding
-    }
-  }, [text])
-
-  return {
-    handleClick,
-    opened,
-    closing,
-    width,
-    textContentRef,
-  }
+const widthVariants = {
+  visible: {
+    width: 'auto',
+    transition: { stiffness: 500 },
+  },
+  hidden: {
+    width: 0,
+    transition: { stiffness: 500 },
+  },
 }
 
 export const Badge = React.forwardRef(
-  ({ icon, text, count, onClick, open, ...props }, forwardedRef) => {
-    const {
-      opened,
-      closing,
-      width,
-      handleClick,
-      textContentRef,
-    } = useTransition(open, text, onClick)
+  (
+    {
+      icon,
+      text,
+      open: controlledOpen,
+      size,
+      reverse,
+      count,
+      onClick,
+      onAnimationComplete,
+    },
+    forwardedRef
+  ) => {
+    const controlled = controlledOpen !== undefined
+    const [open, setOpen] = useState(controlled ? controlledOpen : false)
+    const [textEllipsis, setTextEllipsis] = useState(false)
+
+    useEffect(() => {
+      if (controlled && open !== controlledOpen) {
+        setOpen(o => !o)
+      }
+    }, [controlledOpen, controlled, open])
+
+    const textRef = useCallback(node => {
+      if (node !== null) {
+        setTextEllipsis(node.clientWidth < node.scrollWidth)
+      }
+    }, [])
+
+    const handleClick = useCallback(
+      e => {
+        if (!controlled) {
+          setOpen(o => !o)
+        }
+        if (onClick) {
+          onClick(e)
+        }
+      },
+      [onClick, controlled]
+    )
 
     return (
       <StyledBadge
-        {...props}
-        ref={forwardedRef}
-        text={text}
+        role="button"
+        tabIndex={0}
         onClick={handleClick}
-        opened={opened}
+        ref={forwardedRef}
+        size={size}
+        reverse={reverse}
       >
-        {closing && <Icon {...props} icon="close" color="paperWhite" />}
+        <AnimatePresence exitBeforeEnter initial={false}>
+          <IconContainer
+            size={size}
+            reverse={reverse}
+            key={open ? 'open' : 'closed'}
+          >
+            <motion.div
+              variants={opacityVariants}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              onAnimationComplete={!text && onAnimationComplete}
+            >
+              {open ? (
+                <Icon icon="close" size={size} color="paperWhite" />
+              ) : (
+                icon && (
+                  <Icon
+                    icon={icon}
+                    size={size}
+                    text={text}
+                    color="paperWhite"
+                  />
+                )
+              )}
+            </motion.div>
+          </IconContainer>
+        </AnimatePresence>
 
-        {!closing && icon && (
-          <StyledIcon {...props} icon={icon} text={text} color="paperWhite" />
-        )}
-        <StyledTextWrapper opened={opened} width={width}>
-          <div ref={textContentRef}>
-            <Paragraph {...props} padded={false} color="paperWhite" bold>
+        {text && (
+          <TextContainer
+            size={size}
+            reverse={reverse}
+            icon={icon}
+            variants={widthVariants}
+            initial={open ? 'hidden' : 'visible'}
+            animate={open ? 'hidden' : 'visible'}
+            onAnimationComplete={text && onAnimationComplete}
+          >
+            <Text
+              size={size}
+              padded={false}
+              color="paperWhite"
+              bold
+              ref={textRef}
+              ellipsis={textEllipsis}
+            >
               {text}
-            </Paragraph>
-          </div>
-        </StyledTextWrapper>
+            </Text>
+          </TextContainer>
+        )}
 
         {count > 0 && (
-          <StyledCount
-            {...props}
-            width={width}
-            opened={opened}
-            closing={closing}
+          <Count
+            variants={opacityVariants}
+            animate={open ? 'hidden' : 'visible'}
+            size="mini"
+            color="paperWhite"
+            bold
+            padded={false}
           >
-            <Paragraph size="mini" color="paperWhite" bold padded={false}>
-              {count}
-            </Paragraph>
-          </StyledCount>
+            {count > 99 ? '99+' : count}
+          </Count>
         )}
       </StyledBadge>
     )
@@ -173,7 +240,7 @@ Badge.propTypes = {
    */
   count: PropTypes.number,
   /**
-   * Specify whether badge is open
+   * Specify whether badge is open, this will turn the component into a controlled component, and you will be responsible for controlling the open state through clicks, etc
    */
   open: PropTypes.bool,
   /**
@@ -184,6 +251,10 @@ Badge.propTypes = {
    * Specify the onClick handler
    */
   onClick: PropTypes.func,
+  /**
+   * Specify the onAnimationComplete handler
+   */
+  onAnimationComplete: PropTypes.func,
 }
 
 Badge.defaultProps = {
@@ -191,7 +262,8 @@ Badge.defaultProps = {
   size: 'big',
   text: null,
   count: 0,
-  open: false,
+  open: undefined,
   reverse: false,
-  onClick: null,
+  onClick: undefined,
+  onAnimationComplete: undefined,
 }
